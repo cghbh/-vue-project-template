a1e8c6caa6e84ac043ae6f54baf07208a8dfe4f3
cghbh <1670755868@qq.com>
2021-11-09 13:55:38 +0800

    vant+js的模板完成

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <van-button type="primary">Button</van-button>
+      <van-button style="margin-left: 20px" type="warning">Button</van-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js + Vant + Javascript App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
